13978c9c0258c922d4afbff5e133336166caeb34
Aman Rai <arai24197@gmail.com>
2020-12-26 20:25:11 +0530

    add some changes

---
+++ client/src/component/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import { Header,Landing } from './';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="bg-black">
+        <Header />
+        <Landing />
+      </div>
+    );
+  }
