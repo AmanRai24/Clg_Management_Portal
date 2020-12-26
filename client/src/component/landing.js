d35e5d0828b737e218e2908def1ae37e538dd5f9
Aman Rai <arai24197@gmail.com>
2020-12-26 20:07:45 +0530

    created header and landing page ui

---
+++ client/src/component/landing.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link,Redirect } from 'react-router-dom';
+
+class landing extends Component {
+    render() {
+        return (
+            <div className="landing-container">
+                <div className="description">
+                    <span className="heading">College Portal</span>
+                    <div className="button">
+                        <button type='button' className="btn btn-primary m-2 mt-5 btn-lg">Teacher</button>
+                        <button  type='button' className="btn btn-secondary m-2 mt-5 btn-lg">Student</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default landing; 
