2ddcc47fc6a7499557fdc598622bebe8189d1604
Aman Rai <arai24197@gmail.com>
2020-12-27 18:23:43 +0530

    some changes

--- controllers/teacher_controller.js
+++ controllers/teacher_controller.js
@@ -1,30 +1,35 @@
-const Seller = require('../models/teacher');
+const teacher = require('../models/teacher');
 const bcrypt = require('bcryptjs');
 
 module.exports.register = async function(req,res){
     try {
+        // console.log(req.body);
         if (!req.body.email || !req.body.name || !req.body.password || !req.body.confirmPassword){
             return res.status(404).json({
                 message: "Enter valid text"
             });
         }else {
-            let seller = await Seller.findOne({email: req.body.email});
-            if(!seller){
+            let teacher = await teacher.findOne({email: req.body.email});
+            if(!teacher){
                 let salt = await bcrypt.genSalt(10);
                 let hash = await bcrypt.hash(req.body.password, salt);
-                let newSeller = await Seller.create({
+                let newteacher = await teacher.create({
                     email: req.body.email,
                     name: req.body.name,
                     password: hash
                 });
-                newSeller.save();
+                newteacher.save();
+                console.log('newteacher',newteacher);
                 return res.status(200).json({
-                    message: "Seller Register Successfully!",
+                    success: true,
+                    message: "teacher Register Successfully!",
+                    id:  newteacher._id,
                 });
             }
             else{
-                return res.status(200).json({
-                    message: "Already exist",
+                return res.status(208).json({
+                    success: false,
+                    message: "User already exist",
                 }); 
             }
         }
@@ -38,22 +43,25 @@ module.exports.register = async function(req,res){
 
 module.exports.login = async function(req,res){
     try {
-        console.log(req.body);
-        let seller = await Seller.findOne({email: req.body.email})
-        console.log(seller.email);
-        if(!seller){
+        // console.log(req.body);
+        let teacher = await teacher.findOne({email: req.body.email})
+        // console.log(teacher.email);
+        if(!teacher || !bcrypt.compareSync(req.body.password,teacher.password)){
             return res.status(422).json({
+                success:false,
                 message: "Invalid Username/Password",
             });
         }else{
             return res.status(200).json({
-                message: "Sign in successfully, here is your token, please keep it safe",
-                Seller: { seller }
+                success: true,
+                message: "Sign in successfully",
+                id:  teacher._id
             });
         }
     }catch (err) {
         return res.status(500).json({
             message: "Internal Server Error",
+            err: err
         });
     }
 }
