e10f194ce7e8e356fc1ee21100e2f2b88709340d
Aman Rai <arai24197@gmail.com>
2020-12-28 19:04:54 +0530

    add assignment schema

---
+++ models/assignment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const assignmentSchema = new mongoose.Schema(
+    {   assignment: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        deadline:{
+            type:String,
+            required:true
+        },
+        description: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Assignment = mongoose.model('Assignment', assignmentSchema);
+module.exports = Assignment; 
