2167eacd3d18f2f316f0060a7270339171fac43e
Aman Rai <arai24197@gmail.com>
2020-12-28 17:26:43 +0530

    display all assignment

---
+++ client/src/component/teacher/allAssignment.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+//  demo for see all assignment
+class allAssignment extends Component {
+    render() {
+        return (
+
+            <div className="card" style={{width: "18rem",margin:"5px 5px 5px 5px",display:"inline-block"}}>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+
+        );
+    }
+}
+
+export default allAssignment; 
